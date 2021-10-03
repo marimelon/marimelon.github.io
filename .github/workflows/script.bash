f77338b75b54dbc8952d7c23aaf8d2500ff9093b
marimelon <29140293+marimelon@users.noreply.github.com>
2021-10-04 01:19:27 +0900

    add script.bash

---
+++ .github/workflows/script.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function get_url () {
+    echo "https://marimelon.github.io/knowledge/${1#./}" | sed -e 's/.md//g'
+}
+
+for file in `\find . -name '*.md'`; do
+    HEAD_LINE_NUM=$(sed -n '/^# /=' $file | head -n 1 -)
+    HEAD_LINE=$(sed -n ${HEAD_LINE_NUM}p $file)
+    if [[ ! $HEAD_LINE =~ "http" ]]; then
+      URL=`get_url $file`
+      sed -i -e "$HEAD_LINE_NUM s/^# \([^\r\n]*\).*/# \[\1\](${URL//\//\/})/g" $file
+    fi
+done
